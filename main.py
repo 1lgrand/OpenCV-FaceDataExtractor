be9568b3ce6b5195a53c8d29f2ae7c0c2850a763
Ilgrand <demarzoad@libero.it>
2023-05-31 10:34:07 +0200

    Commenti

--- main.py
+++ main.py
@@ -26,20 +26,18 @@ def get_color_name(color_value):
     else:
         return "Biondo platino"
 
+
+# Calcolo dei colori seguendo i connotati
 for (x, y, w, h) in faces:
     face = image[y:y+h, x:x+w]
     
-    # Calcola il colore dei capelli
     hair_color = np.mean(face[:, :, 2])
     
-    # Calcola il colore degli occhi
     eye_roi = face[int(h/2):h, :]
     eye_color = np.mean(eye_roi)
     
-    # Calcola il colore della pelle
     skin_roi = face[int(h/2):h, int(w/4):int(3*w/4)]
     skin_color = np.mean(skin_roi)
     
-    # Stampa i risultati
     print("Colore dei capelli: ", get_color_name(int(hair_color)))
     print("Colore degli occhi: ", get_color_name(int(eye_color)))
